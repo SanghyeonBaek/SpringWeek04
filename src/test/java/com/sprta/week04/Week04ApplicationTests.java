80449dc6bc8f457bf862cd22899ec62c92fe6eb2
Sanghyeon Baek <baek1389@gmail.com>
2022-02-16 22:25:43 +0900

    [ADD] spring week05

--- src/test/java/com/sprta/week04/Week04ApplicationTests.java
+++ src/test/java/com/sprta/week04/Week04ApplicationTests.java
@@ -3,11 +3,3 @@ package com.sprta.week04;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Week04ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
